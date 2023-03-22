96c9c97b69d55160862388da32160b1abdca58fc
David Podzimek <davidpodzimek1@gmail.com>
2023-03-22 13:16:58 +0100

    accounts database deployed

--- SunPortal.Cloud.Services.Portal/Data/ApplicationDbContext.cs
+++ SunPortal.Cloud.Services.Portal/Data/ApplicationDbContext.cs
@@ -5,8 +5,9 @@ namespace SunPortal.Cloud.Services.Portal.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
     }
 }
