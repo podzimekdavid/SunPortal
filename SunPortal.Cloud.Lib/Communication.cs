952c57d39649295df050f1ec0758388467c2ed08
David Podzimek <davidpodzimek1@gmail.com>
2023-03-24 19:55:11 +0100

    DEBUG connection

--- SunPortal.Cloud.Lib/Communication.cs
+++ SunPortal.Cloud.Lib/Communication.cs
@@ -1,3 +1,4 @@
+#define DEBUG
 namespace SunPortal.Cloud.Lib;
 
 public class Communication
@@ -6,10 +7,15 @@ public class Communication
     public const string RMQ_RESPONSE_CHANNEL = "ValueRespons";
     public const string RMQ_CHANGE_PARAMETER_REQUEST_CHANNEL = "ChangeParameterReguests";
     public const string RMQ_CONFIG_PREFIX = "RabbitMQ:";
-    
+
     public class Endpoints
     {
+#if DEBUG
+        public const string BASE = "http://10.30.1.20:7799";
+#else
         public const string BASE = "http://database";
+#endif
+
         public const string CLIENTS = "/clients";
         public const string PARAMETERS = "/device/parameters/";
         public const string DEVICES = "/devices/";
